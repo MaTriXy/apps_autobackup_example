67009cfa068aa9f5b7d4f3424843060ec2020c4e
Yossi Elkrief <elkriefy@gmail.com>
2015-10-25 23:55:42 +0200

    init

---
+++ Application/src/main/java/com/elkriefy/android/apps/autobackupexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.elkriefy.android.apps.autobackupexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+
+Android Automatic Backup Sample
+===================================
+
+Sample demonstrating Android Marshmallow apps auto backup feature.
+
+
+Api requirements
+--------------
+
+- Android SDK v23
+- Android Build Tools v23.0.1
+- Android Support Repository
